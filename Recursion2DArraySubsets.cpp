e3206a4a79430c794776ceab991074733c8a968c
jahnavimehta <97538596+jahnavimehta@users.noreply.github.com>
2022-10-20 23:57:18 +0530

    2D array subsets

---
+++ Recursion2DArraySubsets.cpp
@@ -0,0 +1,51 @@
+//Form subsets containg the elements of the 2D input array and return them. 
+//Provided you mainatin the sequence of elemnts of the original input array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int arraySubset(int arr[], int n, int output[][20]){
+    //base case
+  if(n==0){
+    output[0][0]=0;
+    return 1;
+  }
+  
+  //smaller recursive call
+  int size= arraySubset(arr+1,n-1,output);
+  
+  //Our calculation
+  for(int i=0;i<size;i++)
+  {
+    int k=output[k][0];
+    output[size+i][0]=k+1;
+    output[size+i][1]=arr[0];
+    
+    for(int j=0;j<k;j++){
+      output[size+i][j+2]=output[i][j+1];
+    }
+  }
+  return size*2;
+  
+}
+    
+
+int main() {
+  int arr[20],length, output[35000][20];
+  cin >> length;
+  for(int i=0; i < length; i++)
+    cin >> arr[i];
+  
+  int size = arraySubset(arr, length, output);
+
+  for( int i = 0; i < size; i++) { 
+	for( int j = 1; j <= output[i][0]; j++) { 
+		cout << output[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+
+  
+  
