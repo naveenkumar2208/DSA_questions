f7b37211a96f76d55a148501f92858111b9d8e9e
jahnavimehta <97538596+jahnavimehta@users.noreply.github.com>
2022-10-22 13:37:54 +0530

    Update Recursion2DArraySubsets.cpp

--- Recursion2DArraySubsets.cpp
+++ Recursion2DArraySubsets.cpp
@@ -1,51 +1,42 @@
 //Form subsets containg the elements of the 2D input array and return them. 
 //Provided you mainatin the sequence of elemnts of the original input array
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int arraySubset(int arr[], int n, int output[][20]){
-    //base case
-  if(n==0){
-    output[0][0]=0;
-    return 1;
+int subset(int input[], int n, int output[][20]){
+ if(n<=0) {
+      output[0][0]=0;
+      return 1;
   }
+
+  int smallOutput = subset(input+1,n-1,output);
   
-  //smaller recursive call
-  int size= arraySubset(arr+1,n-1,output);
-  
-  //Our calculation
-  for(int i=0;i<size;i++)
-  {
-    int k=output[k][0];
-    output[size+i][0]=k+1;
-    output[size+i][1]=arr[0];
-    
-    for(int j=0;j<k;j++){
-      output[size+i][j+2]=output[i][j+1];
-    }
+  for(int i=0;i<smallOutput;i++) {
+      int col = output[i][0] +1;
+     output[i+smallOutput][0] = col;
+       output[i+smallOutput][1] = input[0];
+      for(int j=2; j<col+1;j++) {
+        output[i+smallOutput][j] = output[i][j-1];
+      }
+
   }
-  return size*2;
-  
+
+  return 2*smallOutput;
 }
-    
+
 
 int main() {
-  int arr[20],length, output[35000][20];
+  int input[20],length, output[1000][20];
   cin >> length;
   for(int i=0; i < length; i++)
-    cin >> arr[i];
+    cin >> input[i];
   
-  int size1 = arraySubset(arr, length, output);
+  int size = subset(input, length, output);
 
-  for( int i = 0; i < size1; i++) { 
+  for( int i = 0; i < size; i++) { 
 	for( int j = 1; j <= output[i][0]; j++) { 
 		cout << output[i][j] << " ";
     }
     cout << endl;
   }
 }
-
-
-  
-  
