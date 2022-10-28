1d4ae7a05470bb129586ac44c5e8fae02fb160d2
Gaonkar1039 <gaonkar.1@iitj.ac.in>
2022-10-28 15:11:04 +0530

    Create Container with most water

---
+++ Programs/C++/vector/Container With Most Water.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int maxArea(vector<int>height) {
+        int max_area=0;
+        int area = 0;
+        int left_pointer = 0;
+        int right_pointer = height.size()-1;
+        for(int i = 0;i<height.size()-1; i++){
+            if(height[right_pointer]>height[left_pointer]){
+                area = (right_pointer-left_pointer)*height[left_pointer];
+                if(max_area<area){
+                    max_area = area;
+                }
+                left_pointer +=1;
+
+            }
+            else{
+                
+                area = (right_pointer-left_pointer)*height[right_pointer];
+       
+                if(max_area<area){
+                    max_area = area;
+                }
+                right_pointer-=1;
+                
+            }
+
+        }
+        
+        return max_area;
+        
+    }
+};
